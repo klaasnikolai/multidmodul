c5b4cf9eea3a5083f3bec78c0fa1bddf1dde8a01
Klaas <boedefeld@outlook.com>
2013-06-06 15:48:42 +0200

    added parameter String s.

--- Multimodulproject/src/main/java/main.java
+++ Multimodulproject/src/main/java/main.java
@@ -5,6 +5,7 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String s = "hello";
 		Course c = new Course("4bhif");
 		
 		System.out.println("foo " + c.getName());
