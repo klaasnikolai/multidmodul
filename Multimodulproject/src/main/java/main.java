2bd4dbe44790b5fce213610764b1e74520f6834d
Klaas <boedefeld@outlook.com>
2013-06-06 15:46:22 +0200

    deleted a line.

--- Multimodulproject/src/main/java/main.java
+++ Multimodulproject/src/main/java/main.java
@@ -6,7 +6,7 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		Course c = new Course("4bhif");
-		Teacher t = new Teacher(c.getName(), 1);
+		
 		System.out.println("foo " + c.getName());
 	}
 
